743b14e1cbfcbf4357005f5e671d0f2d89b1668f
El3osa <hhhmohamed91@gmai.com>
2024-08-18 23:49:47 +0300

    create home screen with BottomNavigationBar.

---
+++ lib/home/browes/browes_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class BrowesScreen extends StatelessWidget {
+  const BrowesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/home/movie_home/movie_home.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class MovieHome extends StatelessWidget {
+  const MovieHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
