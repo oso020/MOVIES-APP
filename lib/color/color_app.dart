dca9b044716797ab93b7958dbaef0075674b5c55
El3osa <hhhmohamed91@gmai.com>
2024-08-18 22:26:05 +0300

    start point.

---
+++ lib/color/color_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorApp{
+  static const Color primaryColor=Color(0xffFFBB3B);
+  static const Color lightGreyColor=Color(0xffC6C6C6);
+  static const Color mediumGreyColor=Color(0xffC6C6C6);
+  static const Color darkGreyColor=Color(0xff121312);
+  static const Color whiteColor=Color(0xffffffff);
+  static const Color backgroundColor=Color(0xff121312);
+}
---
+++ lib/theme/theme_app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie_app/color/color_app.dart';
+
+class ThemeApp{
+
+  static final ThemeData theme=ThemeData(
+    scaffoldBackgroundColor: ColorApp.backgroundColor,
+
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: ColorApp.darkGreyColor,
+      selectedItemColor: ColorApp.primaryColor,
+      unselectedItemColor: ColorApp.lightGreyColor,
+      type: BottomNavigationBarType.fixed
+    ),
+
+    textTheme: TextTheme(
+      titleSmall:GoogleFonts. inter(
+          fontSize: 15,
+          color: ColorApp.whiteColor,
+      ),
+      titleMedium:GoogleFonts.poppins(
+        fontSize: 10,
+        color: ColorApp.whiteColor,
+      ),
+
+
+
+    )
+
+
+
+  );
+}
