bc73a6a36abc1b52d71249b0d25b7014ffc1a2ec
El3osa <hhhmohamed91@gmai.com>
2024-08-19 23:11:07 +0300

    added details to item recommeded.

--- lib/home/movie_home/movie_item_recommeded.dart
+++ lib/home/movie_home/movie_item_recommeded.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movie_app/color/color_app.dart';
 
 class MovieItemRecommeded extends StatefulWidget {
   const MovieItemRecommeded({super.key});
@@ -18,31 +20,72 @@ class _MovieItemRecommededState extends State<MovieItemRecommeded> {
           borderRadius: BorderRadius.circular(5),
           child: Image.asset(
             "assets/images/test.jpg",
-            width: 100.w,
+            width: 90.w,
             height: 170.h,
             fit: BoxFit.fill,
           ),
         ),
+        GestureDetector(
+          onTap: () {
+            isBooked = !isBooked;
+            setState(() {});
+          },
+          child: isBooked == true
+              ? Image.asset(
+                  "assets/images/bookmark_saved.png",
+                  width: 30.w,
+                  height: 40.h,
+                  fit: BoxFit.fill,
+                )
+              : Image.asset(
+                  "assets/images/bookmark.png",
+                  width: 30.w,
+                  height: 40.h,
+                  fit: BoxFit.fill,
+                ),
+        ),
         Positioned(
-          left: 2.w,
-          child: GestureDetector(
-            onTap: () {
-              isBooked = !isBooked;
-              setState(() {});
-            },
-            child: isBooked == true
-                ? Image.asset(
-                    "assets/images/bookmark_saved.png",
-                    width: 30.w,
-                    height: 40.h,
-                    fit: BoxFit.fill,
-                  )
-                : Image.asset(
-                    "assets/images/bookmark.png",
-                    width: 30.w,
-                    height: 40.h,
-                    fit: BoxFit.fill,
+          top: 103.h,
+          child: Container(
+            padding: EdgeInsets.all(7),
+            decoration: BoxDecoration(
+              color: Color(0xff514F4F),
+borderRadius:BorderRadius.only(bottomRight: Radius.circular(5),bottomLeft: Radius.circular(5))
+            ),
+            width: 90.w,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/star-2.png",
+                      width: 15.w,
+                      height: 15.h,
+                      fit: BoxFit.fill,
+                    ),
+                    SizedBox(
+                      width: 3.w,
+                    ),
+                    Text(
+                      "7.7",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    )
+                  ],
+                ),
+                Text(
+                  "Deadpool 2",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Text(
+                  "2019  PG-13  2h 7m",
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    fontSize: 8.sp,
+                    color: ColorApp.lightGreyColor,
                   ),
+                ),
+              ],
+            ),
           ),
         )
       ],
