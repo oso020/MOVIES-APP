8608063dc03d5902b724e1d3e387ef4e5df9bab1
ZiadElshal <zezoelzamalkawy14@gmail.com>
2024-08-21 07:12:47 +0300

    Finished designed Ui of Watchlist Screen.

--- lib/home/watch_list/watch_list_screen.dart
+++ lib/home/watch_list/watch_list_screen.dart
@@ -1,11 +1,54 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movie_app/color/color_app.dart';
+import 'package:movie_app/home/watch_list/watch_list_item.dart';
 
 class WatchListScreen extends StatelessWidget {
   const WatchListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 12.h,
+          ),
+          Row(
+            children: [
+              Icon(Icons.bookmark_add_outlined, color: ColorApp.primaryColor, size: 30,),
+              SizedBox(
+                width: 5.w,
+              ),
+              Text('Watch List',
+                style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    fontSize: 24.sp,
+                    color: ColorApp.whiteColor
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+
+          Expanded(
+            child: ListView.separated(
+              separatorBuilder: (context, index) {
+                return Divider(
+                  height: 4,
+                );
+              },
+              itemBuilder: (context, index){
+                return WatchListItem();
+              },
+              itemCount: 10,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
