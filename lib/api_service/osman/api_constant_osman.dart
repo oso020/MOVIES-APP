a2c79e92f4825325f7a477a48b24b1153a63e8f0
El3osa <hhhmohamed91@gmai.com>
2024-08-22 20:56:26 +0300

    fix some problem.

--- lib/api_service/osman/api_constant_osman.dart
+++ lib/api_service/osman/api_constant_osman.dart
@@ -5,7 +5,6 @@ class ApiConstant {
   static const String apiLinkRecomended = "/3/movie/top_rated";
   static const String authorization =
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjZmMDE0M2JlMDkxMzc0MTY2NDJkNmExMzY3NjJhOSIsIm5iZiI6MTcyNDI1ODI4OC40MjU5MTQsInN1YiI6IjY2YzYxNjY4NTk2MWNlZTg3ZTY5ZWQyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kE8SIKc1Fd0bVmdFRIJP9yYX0zqA3suUj-aDYE976nw";
-
   static const String accept = "application/json";
   static const String imageBaseUrl = "https://image.tmdb.org/t/p/w500";
 }
